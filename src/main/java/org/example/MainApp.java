0ee3e5f6387ca4917f7cc41efa1ed30984234c09
Dermot Logue <dermot.logue@dkit.ie>
2024-09-16 12:16:57 +0100

    comment updates

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,11 +1,10 @@
-package org.example;
+package org.example;            // Updated: Sept 2024
 
 import java.util.Random;
 
 /**
  * Multi-Dimensional Arrays.
- * Sample code to recap on one-dimensional arrays and to   (2022)
- * demonstrate Two-Dimensional Arrays (2D Arrays)
+ * Demonstrates: Two-Dimensional Arrays (2D Arrays)
 */
 
 public class MainApp {
@@ -16,47 +15,15 @@ public class MainApp {
     }
 
     public void start() {
-        demoArray();
         demo2DArray();
-    }
-
-    public void demoArray() {
-
-        int[] array1 = {2,4,6,8,10};    // initial array with values
-
-        for(int i=0; i<array1.length; i++) {
-            System.out.print(array1[i]+",");
-        }
-        System.out.println();
-
-        // pass array into a method
-        display( array1 );
-
-        int[] array2 = new int[10];  // create array with 10 elements
-        populate(array2);
-        display(array2);
-
-        //TODO  - write the method sumArray(), and test it.
-        //int sum = sumArray( array1 );
-    }
-
-    public void display( int[] arr ) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]+",");
-        }
-        System.out.println();
-    }
-
-    public void populate( int[] arr) {      // arr is a reference to an array of int
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = (int) (Math.random() * 100) + 1; // random number 1-100
-        }
     }
 
     public void demo2DArray(){
 
-        // create 2x2 2D array
+        // create 2x2 2D array - ( an array of arrays)
         int[][] arr = new int[2][2];    // 2 rows x 2 columns
+
+        // 2D arrays have a fixed size - i.e. once created they can not be made bigger or smaller
 
         // assign values to each element
         arr[0][0] = 3;      // row 0, column 0
@@ -64,8 +31,8 @@ public class MainApp {
         arr[1][0] = 7;
         arr[1][1] = 9;
 
-        for(int i=0; i<2; i++) {        // row (outer loop)
-            for (int j=0; j<2; j++)         // column (inner loop)
+        for(int i=0; i<arr.length; i++) {        // row (outer loop)
+            for (int j=0; j< arr[0].length; j++)         // column (inner loop)
                 System.out.print(arr[i][j]+", ");    // element arr[row][col]
             System.out.println();   // move to next line
         }
@@ -73,38 +40,38 @@ public class MainApp {
         // sudukoBox is a 2D Array
 
         int[][] sudukoBox = {   {2,4,9},    // initializer -
-                                {7,1,8},    // this will be a 3x3 array
-                                {3,5,6}};
+                                {7,1,8},    // this will create a 3x3 array
+                                {3,5,6}
+        };
 
         for( int i=0; i< sudukoBox.length; i++) {   // rows
-
             for (int j = 0; j < sudukoBox[0].length; j++)  // columns
                 System.out.print(sudukoBox[i][j] +", ");
 
             System.out.println();
         }
 
-        // TODO
-        // write the method sum2DArray that accepts a 2D array as an argument
+        //TODO
+        // Write the method sum2DArray that accepts a 2D array as an argument
         // sums up all the elements, and returns the sum
         //
         //int sum = sum2DArray(sudukoBox);
 
         //TODO
-        //Write code to compare the elements of these 2D arrays
-        // output the positions where the elements do not match
-        // e.g "No match at position [1][2]"
+        // Write code to compare the elements of these 2D arrays.
+        // Output the positions where the elements do not match
+        // e.g "No match at position [0][2], [1][1]"
 
         int[][] arrA = {   {2,4,9},    // initializer -
                             {7,1,8},    // this will be a 3x3 array
                             {3,5,6}};
-        int[][] arrB = {   {2,4,9},    // initializer -
+        int[][] arrB = {   {2,4,2},    // initializer -
                             {7,0,8},    // this will be a 3x3 array
                             {3,5,6}};
 
         // add code here to undertake matching.
 
         // When finished, move the code into a method but this time simply return
-        // "true" or "false" to indicate whether the 2D arrays match or not overall
+        // "true" or "false" to indicate whether the two 2D arrays match overall or not
     }
 }
